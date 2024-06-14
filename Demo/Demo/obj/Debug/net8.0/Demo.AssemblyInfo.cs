fa1d0c639e9e80204bcb6320da65faf64218ccd9
Linhkk01tb <dragmedown113@gmail.com>
2024-06-14 19:58:09 +0700

    Add-Test-images

--- Demo/Demo/obj/Debug/net8.0/Demo.AssemblyInfo.cs
+++ Demo/Demo/obj/Debug/net8.0/Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+409882cc3b96da060a23f4ffd149151cee9c3a82")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
